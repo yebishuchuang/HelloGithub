491085a2c8911baa8cda729a0b43d6fd60264352
xgaifh <xgaifh@163.com>
2020-08-26 16:01:06 +0800

    新增一个HelloGithub类

---
+++ HelloGithub/src/com/fh/demo/HelloGithub.java
@@ -0,0 +1,13 @@
+package com.fh.demo;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author fh
+ * @create 2020-08-26 下午3:56
+ */
+public class HelloGithub {
+    public static void main(String[] args) {
+        System.out.println("Hello Github!");
+    }
+}
